a96608eb42c4d74276ee8c4145ad8e1e3364b1a3
Matthieu Dorier <mdorier@anl.gov>
2023-12-14 14:51:27 +0000

    added yokan and warabi to bedrock config in tests

--- tests/BedrockConfig.hpp
+++ tests/BedrockConfig.hpp
@@ -9,13 +9,37 @@
 static inline const char* config = R"(
 {
     "libraries" : {
-        "mofka" : "libmofka-bedrock-module.so"
+        "mofka" : "libmofka-bedrock-module.so",
+        "warabi" : "libwarabi-bedrock-module.so",
+        "yokan" : "libyokan-bedrock-module.so"
     },
     "providers" : [
         {
             "name" : "my_mofka_provider",
             "type" : "mofka",
             "provider_id" : 0
+        },
+        {
+            "name" : "my_warabi_provider",
+            "type" : "warabi",
+            "provider_id" : 1,
+            "config" : {
+                "target" : {
+                    "type": "memory",
+                    "config": {}
+                }
+            }
+        },
+        {
+            "name" : "my_yokan_provider",
+            "type" : "yokan",
+            "provider_id" : 2,
+            "config" : {
+                "database" : {
+                    "type": "map",
+                    "config": {}
+                }
+            }
         }
     ],
     "ssg" : [
