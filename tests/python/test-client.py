2094c68e80d90d042c651596747f9395a0a9dd40
GueroudjiAmal <ea_gueroudji@esi.dz>
2024-02-08 07:27:23 -0600

    some tests

--- tests/python/test-client.py
+++ tests/python/test-client.py
@@ -30,28 +30,44 @@ class TestClient(unittest.TestCase):
     def test_create_client(self):
         """Test client creation."""
         client = mofka.Client(mid=self.mid)
-    """
+
     def test_connect_client(self):
         client = mofka.Client(mid=self.mid)
         ssg_file = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "mofka.ssg")
         service = client.connect(ssg_file)
-    """    
+ 
     def test_get_config(self):
         client = mofka.Client(mid=self.mid)
         client.get_config()
 
 class TestThreadPool(unittest.TestCase):
+
     def test_create_thread_pool(self):
-        self.count = random.randint(1, 64)
-        self.thread_pool = mofka.ThreadPool(self.count)
-        print(self.count)
-        print(self.thread_pool)
+        count = random.randint(1, 64)
+        thread_pool = mofka.ThreadPool(count)
 
     def test_get_thread_count(self):
-        count = self.thread_pool.thread_count()
+        count = random.randint(1, 64)
+        thread_pool = mofka.ThreadPool(count)
+        count1 = thread_pool.thread_count()
+        assert(count == count1)
 
-        
+class TestServiceHandle(unittest.TestCase):
+    def setUp(self):
+        bedrock_config_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "config.json")    
+        with open(bedrock_config_file) as f:
+            self.bedrock_server = BedrockServer("na+sm", config=f.read())
+        self.mid = self.bedrock_server.margo.mid
+        self.client = mofka.Client(mid=self.mid)
+        ssg_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "mofka.ssg")
+        self.service = client.connect(ssg_file)
+
+    def tearDown(self):
+        self.bedrock_server.finalize()   
+
 
 if __name__ == '__main__':
     unittest.main()
