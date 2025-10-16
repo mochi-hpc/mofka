14988cd4cea0003f5c1d0b5def4bf292749467dd
Matthieu Dorier <mdorier@anl.gov>
2025-10-16 10:33:42 +0100

    updated post-test.sh to display the content of the Mofka log files

--- tests/post-test.sh
+++ tests/post-test.sh
@@ -7,14 +7,28 @@ echo "================================================"
 echo "==> Shutting down server"
 BEDROCK_PID=$(<mofka.pid)
 
+show_logs=0
+
 if kill -0 $BEDROCK_PID 2>/dev/null; then
     kill $BEDROCK_PID
+    sleep 1
 else
-    echo "Mofka isn't running anymore, here are the logs:"
-    cat mofka.err mofka.out
+    echo "Mofka isn't running anymore when it should"
+    show_logs=1
 fi
 
-sleep 1
+if [ $1 -ne "0" ]; then
+    echo "The test didn't seem to succeed"
+    show_logs=1
+fi
+
+if [ $show_logs -ne 0 ]; then
+    echo "Mofka stderr ===================================="
+    cat mofka.err
+    echo "Mofka stdout ===================================="
+    cat mofka.out
+fi
+
 rm mofka.json mofka.pid
 
 echo "==> Removing log directory"
